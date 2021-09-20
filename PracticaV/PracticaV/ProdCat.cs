4acaa5d193cfb0e38c5f7aa951795dea38e56892
Naty Francisco <fnaty629@gmail.com>
2021-09-20 19:38:46 -0400

    Practica V
    Consultas con Linq(Query)

---
+++ PracticaV/PracticaV/ProdCat.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PracticaV
+{
+    public class ProdCat
+    {
+
+        public int ProductoId { get; set; }
+        public int CategoriaId { get; set; }
+        public string NombreDelProducto { get; set; }
+        public string NombreDeLaCategoria { get; set; }
+
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+
+        public double Precio { get; set; }
+    }
+}
