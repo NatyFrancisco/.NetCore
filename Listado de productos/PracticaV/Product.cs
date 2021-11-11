35864793d17a4c29035f02b26fd2efcfaa0daea1
Naty Francisco <fnaty629@gmail.com>
2021-11-11 11:38:20 -0400

    Proyecto de listado de productos
    Cree una entidad llamada Producto, con propiedades id, nombre, categoriaId, marca, modelo y precio.

---
+++ Listado de productos/PracticaV/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PracticaV
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int CategoriaId { get; set; }
+
+        public string Marca { get; set; }
+        public string Modelo  { get; set; }
+
+        public double Precio  { get; set; }
+
+       
+    }
+}
